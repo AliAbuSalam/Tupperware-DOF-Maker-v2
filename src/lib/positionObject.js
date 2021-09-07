47f994279862ffa00c4eda263ab1ad8d33254dcd
Ali Abu Salam <aliadadehmav@gmail.com>
2021-09-07 12:06:36 +0700

    Change positionObjects value to match server side

--- src/lib/positionObject.js
+++ src/lib/positionObject.js
@@ -1,14 +1,14 @@
 const positionObjects = [
-  { key: 1, value: 'Director_(Diamond)', text: 'Director_(Diamond)'},
-  { key: 2, value: 'Director_(Gold)', text: 'Director_(Gold)'},
-  { key: 3, value: 'Director_(Silver)', text: 'Director_(Silver)'},
-  { key: 4, value: 'Manager_(5 Star)', text: 'Manager_(5 Star)'},
-  { key: 5, value: 'Manager_(3 Star)', text: 'Manager_(3 Star)'},
-  { key: 6, value: 'Manager_(1 Star)', text: 'Manager_(1 Star)'},
-  { key: 7, value: 'Manager', text: 'Manager'},
-  { key: 8, value: 'Consultant_(SC)', text: 'Consultant_(SC)'},
-  { key: 9, value: 'Consultant_(BB)', text: 'Consultant_(BB)'},
-  { key: 10, value: 'Consultant', text: 'Consultant'},
+  { key: 1, value: 'DIRECTOR_DIAMOND', text: 'Director_(Diamond)'},
+  { key: 2, value: 'DIRECTOR_GOLD', text: 'Director_(Gold)'},
+  { key: 3, value: 'DIRECTOR_SILVER', text: 'Director_(Silver)'},
+  { key: 4, value: 'MANAGER_5_STAR', text: 'Manager_(5 Star)'},
+  { key: 5, value: 'MANAGER_3_STAR', text: 'Manager_(3 Star)'},
+  { key: 6, value: 'MANAGER_1_STAR', text: 'Manager_(1 Star)'},
+  { key: 7, value: 'MANAGER', text: 'Manager'},
+  { key: 8, value: 'CONSULTANT_SC', text: 'Consultant_(SC)'},
+  { key: 9, value: 'CONSULTANT_BB', text: 'Consultant_(BB)'},
+  { key: 10, value: 'CONSULTANT', text: 'Consultant'},
 ];
 
 export default positionObjects;
